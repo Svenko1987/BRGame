624ad7284af18b018376c4e62edd351bc3be9100
Sven <celikovicsven@gmail.com>
2018-04-22 02:10:18 +0200

    Clean up, fix movement, scoutForEnemy
    (cherry picked from commit 9c1a72f)

---
+++ src/com/company/Direction.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public enum Direction {
+    UP (-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1);
+    private final int deltaX;
+    private final int deltaY;
+    Direction(int x, int y ){
+        deltaX=x;
+        deltaY=y;
+    }
+
+    public int getDeltaX() {
+        return deltaX;
+    }
+
+    public int getDeltaY() {
+        return deltaY;
+    }
+}
